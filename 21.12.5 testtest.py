ca7540bb289b36b5a87c5f39dcee7103fc23248d
Steven Wu <89153549+StevenWu1@users.noreply.github.com>
2021-12-11 18:40:49 -0600

    Add files via upload

---
+++ 21.12.5 testtest.py
@@ -0,0 +1,45 @@
+final = []
+answer = []
+answer1 = []
+
+def move(list1, list2, n, actions):
+    if(n <= 0):
+        tank = 1000
+        print("=================",n,"=================")
+        print(list1)
+        print(list2)
+        print(actions)
+        for a in actions:
+            
+            tank = tank + a
+            answer.append(tank)
+        answer1 = list(set(answer))
+        print(answer1)
+        print(tank)
+        print(answer)
+        print(len(answer1))
+    else:
+        list1D = list(dict.fromkeys(list1))
+        for x in list1D:
+            list2.append(x)
+            list1.remove(x)
+            if (n % 2) ==0:
+                actions.append(-x)
+            else:
+                actions.append(x)
+                
+            move(list2, list1, n-1, actions)
+            # give x back
+            list1.append(x)
+            list2.remove(x)
+            if (n%2) == 0:
+                actions.remove(-x)
+            else:
+                actions.remove(x)
+
+
+
+listA = [1, 1, 2]
+listB = [5, 5, 5]
+
+move(listA,listB,4,final)
